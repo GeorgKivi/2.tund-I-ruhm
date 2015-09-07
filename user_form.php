913399a5bfeb6e47f8848e71c9a52f4ac43800bf
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2015-09-07 15:27:28 +0300

    user login form validation

--- user_form.php
+++ user_form.php
@@ -2,6 +2,23 @@
 
     // user_form.php
     
+    // jutumärkide vahele input elemendi NAME
+    //echo $_POST["email"];
+    //echo $_POST["password"];
+    
+    $email_error = "";
+    
+    // kontrolli ainult siis kui kasutaja vajutab logi sisse nuppu
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+        //Kontrollime kasutaja e-posti, et see ei ole tühi
+        if(empty($_POST["email"])) {
+            $email_error = "Ei saa olle tühi";
+        }
+        
+        // Kontrolli parooli
+    
+    }
 
 ?>
 <html>
@@ -11,10 +28,12 @@
     <body>
         
         <h2>Login</h2>
-        <input type="email" placeholder="E-post" > <br><br>
-        <input type="password" placeholder="Parool" > <br><br>
-        
-        <input type ="submit" value="Logi sisse">
+        <form action="user_form.php" method="post">
+            <input name="email" type="email" placeholder="E-post" > <?php echo $email_error; ?> <br><br>
+            <input name="password" type="password" placeholder="Parool" > <br><br>
+            
+            <input type ="submit" value="Logi sisse">
+        </form>
         
         <h2>Create user</h2>
         
