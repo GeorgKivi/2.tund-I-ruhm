9c490fa991287dc5d60561a4944e76d113731917
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2015-09-07 15:51:53 +0300

    added password validation

--- user_form.php
+++ user_form.php
@@ -2,22 +2,35 @@
 
     // user_form.php
     
-    // jutum�rkide vahele input elemendi NAME
+    // jutumärkide vahele input elemendi NAME
     //echo $_POST["email"];
     //echo $_POST["password"];
     
     $email_error = "";
+    $password_error = "";
     
     // kontrolli ainult siis kui kasutaja vajutab logi sisse nuppu
     if($_SERVER["REQUEST_METHOD"] == "POST") {
     
-        //Kontrollime kasutaja e-posti, et see ei ole t�hi
+        //Kontrollime kasutaja e-posti, et see ei ole tühi
         if(empty($_POST["email"])) {
-            $email_error = "Ei saa olle t�hi";
+            $email_error = "Ei saa olle tühi";
         }
         
-        // Kontrolli parooli
-    
+        // Kontrollime parooli
+        if(empty($_POST["password"])) {
+            $password_error = "Ei saa olle tühi";
+        } else {
+            
+            // paroool ei tühi, kontrollime pikkust
+            if(strlen($_POST["password"]) < 8 ){
+                
+                $password_error = "Peab olema vähemalt 8 sümbolit pikk";
+                
+            }
+            
+        }
+        
     }
 
 ?>
@@ -29,8 +42,8 @@
         
         <h2>Login</h2>
         <form action="user_form.php" method="post">
-            <input name="email" type="email" placeholder="E-post" > <?php echo $email_error; ?> <br><br>
-            <input name="password" type="password" placeholder="Parool" > <br><br>
+            <input name="email" type="email" placeholder="E-post" >* <?php echo $email_error; ?> <br><br>
+            <input name="password" type="password" placeholder="Parool" >* <?php echo $password_error; ?><br><br>
             
             <input type ="submit" value="Logi sisse">
         </form>
